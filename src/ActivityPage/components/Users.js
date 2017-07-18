5d20533cb4e3ee5b3936762c51ae3bc9207c1e46
James Lagattuta <jimmy.lagattuta@gmail.com>
2017-07-18 01:16:38 -0700

    React Component User photo & name(connections)

--- src/ActivityPage/components/Users.js
+++ src/ActivityPage/components/Users.js
@@ -4,9 +4,16 @@ import '../css/users.css';
 const Users = (props) => {
   return (
     <div id="activity-users">
-
+    	<div id="bio-pic-users"><img src="https://u.o0bc.com/avatars/no-user-image.gif" alt="" /></div>
+    	<div className="users-name"><p> Mary Swanson</p></div>
     </div>
   );
 }
 
 export default Users;
+
+
+      // <div className="activity-users-content">
+      //   <div id="bio-pic"><img src="https://u.o0bc.com/avatars/no-user-image.gif" alt="" />Mary Swanson</div>
+      //   // <div className="users-name"><p></p></div>
+      // </div>
