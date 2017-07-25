1921878a71416ce240c1fa8fdbc620f773ac04d2
James Lagattuta <jimmy.lagattuta@gmail.com>
2017-07-24 19:02:55 -0700

    trying to save progress,, fixed error message saying that events was undefined in ActivityPage/components/users.js

--- src/ActivityPage/ActivityPage.js
+++ src/ActivityPage/ActivityPage.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
 import Header from '../EventsPage/components/HeaderComponent';
-import Bio from './components/Bio';
+import Bio from '../ActivityPage/components/Bio';
 import Users from './components/Users';
-import YourActivity from './ActivityPage';
+
 
 class ActivityPage extends Component {
+  constructor(props) {
+		super(props);
+		this.state = {
+	    	events: [
+		        {
+		        	host_id:1,
+		      		name:"Mary Swanson",
+		      		photo:"my pix"
+		        },
+		        {
+		        	host_id:2,
+		        	name:"Jane Simton",
+
+		        },
+		        {
+		        	host_id:3,
+		        	name:"Jack Terrier",
+		        	photo:"mypicture"
+		        }]
+    };
+	}
+
 	render () {
 		return (
 			<div>
-				<Header />
-				<Bio />
-				<Users />
-				{/* <YourActivity /> */}
+				< Header />
+				< Bio />
+				< Users events={this.state.events} />
+
 			</div>
 		);
 	}
-};
+}
 
 export default ActivityPage;
--- src/ActivityPage/components/Users.js
+++ src/ActivityPage/components/Users.js
@@ -2,11 +2,38 @@ import React from 'react';
 import '../css/users.css';
 
 const Users = (props) => {
-  return (
-    <div id="activity-users">
+	console.log(props);
 
-    </div>
-  );
+	const userList = props.events.map((user) => {
+		console.log(user);
+  		return (
+    		<div id="activity-users">
+
+    		</div>
+  		);
+	})
 }
 
 export default Users;
+
+
+
+// import React from 'react';
+// import '../css/users.css';
+
+// export default (props) => {
+// 	const userList = props.events.map((user) => {
+// 		console.log(user);
+// 		return (
+//     		<div id="activity-users">
+//     			<div key={user.id} classname="col-xs-offset-1 col-xs-3">
+//     			{user.name}
+//     			{user.photo}
+
+//     			</div>
+//     		</div>
+//  	 	);
+// 	})
+// }	
+
+
