82b5166c73f30c7d63ca9a8622a4cf6d52a54095
Demario Marbray <toosexybuff500@gmail.com>
2017-07-31 18:43:36 -0700

    added the rest of code images facebook

--- src/HomePage/components/HeaderComponent.js
+++ src/HomePage/components/HeaderComponent.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import FacebookLogin from 'react-facebook-login';
 import '../css/header.css';
+import '../css/images.css';
+
+
+
 
 export default (props) => {
   return (
@@ -11,24 +16,20 @@ export default (props) => {
 
 
         <ul className="nav navbar-nav navbar-right">
-          <li>
-
-              <form className="navbar-form">
-                <div className="input-group">
-                  <input type="text" className="form-control" placeholder="Search"/>
-                  <i className="glyphicon glyphicon-search"></i>
-                </div>
-              </form>
-
-          </li>
-          <li><a href="#" className="icon"><i className="fa fa-home"><p>HOME</p></i></a></li>
-          <li><a href="#" className="icon"><i className="fa fa-calendar-o"><p>EVENTS</p></i></a></li>
-          <li><a href="#" className="icon"><i className="fa fa-bell-o"><p>ALERTS</p></i></a></li>
-          <li><a><img className="img-responsive" src="https://u.o0bc.com/avatars/no-user-image.gif" alt="" /></a></li>
+          
+          <li><a href="http://localhost:8080/users/sign_up" className="icon"><i className="fa fa-user-plus"><p>SignUp</p></i></a></li>
+          <li><a href="http://localhost:8080/users/sign_in" className="icon"><i className="fa fa-sign-in"><p>SignIn</p></i></a></li>
+          <li><a href="http://localhost:8080/users/auth/facebook" className="icon"><i className="fa fa-facebook-square" ><p>Facebook Login</p></i></a></li>
         </ul>
       </div>
-
-
     </nav>
+      
+      
+    
   );
 };
+
+ 
+
+
+
