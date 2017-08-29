37822f423c1bb8fcaf101364913969b56d4972a6
James Lagattuta <jimmy.lagattuta@gmail.com>
2017-08-28 20:30:29 -0700

    minor chenges updating to push

--- src/ProfilesPage/components/profile.js
+++ src/ProfilesPage/components/profile.js
@@ -3,21 +3,23 @@ import '../css/profile.css';
 
 
 const Profile = (props) => {
-
-	const query = window.location.pathname;
-	const new_query = query.slice(9);
-	console.log('new query below');
-	console.log(new_query);
+  const { userFeeds } = props;
+  if (userFeeds.length == 0) {
+    return (
+      <div id="feed-none">
+        <p>No Feeds Avaliable</p>
+      </div>
+    )
+  }
 
 
 	return(
-		<div className="container">
-			<div className="box_a">
-				<p id="current-profile"> </p>
-			</div>
+		<div className="box" id="personal-feed">
+				<p>{renderFeeds(props.userFeeds)}</p>
 		</div>
 
 	);
 }
+const renderFeeds = (feeds) => feeds.map((feed) => <div key={feed.postId} feed={feed}/>);
 
 export default Profile;
