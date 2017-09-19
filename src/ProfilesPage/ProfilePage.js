bc885c1c94f4182cdcc11148e0e15ee038883eb8
James Lagattuta <jimmy.lagattuta@gmail.com>
2017-09-19 02:03:03 -0700

    fixing bugs

--- src/ProfilesPage/ProfilePage.js
+++ src/ProfilesPage/ProfilePage.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
-import { fetchUserInfo, fetchUserFeeds, fetchUserConnections, fetchUserEvents, createConnectionRequest, fetchConnectionStatus } from '../actions';
-import Header from '../EventsPage/components/HeaderComponent';
-import Profile from './components/profile';
-import ProfilePhoto from './components/ProfilePhoto';
+import {
+  fetchUserInfo,
+  fetchUserFeeds,
+  fetchUserConnections,
+  createConnectionRequest,
+  fetchConnectionStatus,
+  userWallInputChange,
+  createPostOnUserWall
+} from '../actions';
+import { Navbar, LeftGraySideBar, RightGraySideBar, PageContent } from '../common';
+import UserInfo from './components/UserInfo';
 import ProfileConnections from './components/ProfileConnections';
+import ProfileFeed from './components/ProfileFeed';
 
-
+const userId = sessionStorage.getItem('userId');
 class ProfilePage extends Component {
   componentWillMount() {
       const sender_id = sessionStorage.getItem('userId');
       const receiver_id = this.props.match.params.id;
-      const { fetchUserInfo, fetchUserFeeds, fetchUserConnections, fetchUserEvents } = this.props; 
-      const query = window.location.pathname;
-      const new_query = query.slice(9);
-      fetchUserInfo(new_query);        
-      fetchUserFeeds(new_query);
-      fetchUserConnections(new_query);
-      fetchUserEvents(new_query);
+      const { fetchUserInfo, fetchUserFeeds, fetchUserConnections } = this.props;
+      this.props.fetchUserInfo(this.props.match.params.id);
+      this.props.fetchUserFeeds(this.props.match.params.id);
+      this.props.fetchUserConnections(this.props.match.params.id);
       this.props.fetchConnectionStatus({ sender_id, receiver_id });
     }
 
@@ -29,45 +34,74 @@ class ProfilePage extends Component {
     this.props.createConnectionRequest(data);
   }
 
+  onPost() {
+    const body = this.props.userWallPost;
+    const userId = this.props.match.params.id || sessionStorage.getItem('userId');
+    const authorId = sessionStorage.getItem('userId');
+    this.props.createPostOnUserWall({ body, userId, authorId }, this.props.fetchUserFeeds);
+  }
+
   renderConnection() {
-      if (_.isEmpty(this.props.status)) {
-      return <button type="button" style={{ position: 'relative', top: '300px', left: '300px'}} onClick={this.onPress.bind(this)}>Connect</button>
+    if (this.props.userInfo.id == userId) {
+      return <div></div>
     }
-        if (this.props.status.status === false) {
-      return <div style={{ position: 'relative', top: '300px', left: '300px'}}> Request Pending...</div>
+    if (_.isEmpty(this.props.status)) {
+      return <button type="button"  onClick={this.onPress.bind(this)}>Connect</button>
     }
 
     if (this.props.status.status === true) {
-      return <div style ={{ position: 'relative', top: '300px', left: '300px'}}> Connected </div>
+      return <div> Connected </div>
+    }
+
+    if (this.props.status.status === false) {
+      return <div> Request Pending...</div>
     }
   }
 
   render () {
-      const { userInfo, userConnections, userFeeds, userEvents } = this.props;
-        console.log('this.props below right herrr');
-        console.log( userFeeds );
-
-
     return (
       <div>
-        <Header />
-
-          <ProfilePhoto userInfo={userInfo} userConnections={userConnections} />
-          <Profile userInfo={userInfo} userFeeds={userFeeds} />
-          <ProfileConnections userConnections={userConnections} />
+        <Navbar />
+        <LeftGraySideBar>
+          <UserInfo userInfo={this.props.userInfo}/>
           {this.renderConnection()}
-
+        </LeftGraySideBar>
+        <RightGraySideBar>
+          <ProfileConnections connections={this.props.userConnections}/>
+        </RightGraySideBar>
+        <PageContent>
+          <div className="feed-post-bar">
+            <div className="wrap">
+              <div className="search">
+                <input type="text" className="searchTerm" placeholder="What's New?"
+                  onChange={(event) => this.props.userWallInputChange(event.target.value)}
+                  value={this.props.userWallPost}
+                />
+                <div className="post-button"><button className="btn btn-default" onClick={this.onPost.bind(this)}>POST</button></div>
+              </div>
+            </div>
+          </div>
+          <ProfileFeed feeds={this.props.userFeeds}/>
+        </PageContent>
       </div>
-
     );
   };
-
 }
 
   const mapStateToProps = (state) => {
-    const { userInfo, userFeeds, userConnections, userEvents, status } = state;
-    console.log('status below');
-    console.log(status);
-    return { userInfo, userFeeds, userConnections, userEvents, status };
+    const { userInfo, userFeeds, userConnections, status, userWallPost } = state;
+    console.log(userFeeds);
+
+    return { userInfo, userFeeds, userConnections, status, userWallPost };
   }
-export default connect(mapStateToProps, { fetchUserInfo, fetchUserFeeds, fetchUserConnections, fetchUserEvents, createConnectionRequest, fetchConnectionStatus })(ProfilePage);
+export default connect(mapStateToProps,
+  {
+    fetchUserInfo,
+    fetchUserFeeds,
+    fetchUserConnections,
+    createConnectionRequest,
+    fetchConnectionStatus,
+    userWallInputChange,
+    createPostOnUserWall
+  }
+  )(ProfilePage);
