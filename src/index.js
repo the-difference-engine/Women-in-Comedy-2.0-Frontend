69162808cf98cd3da177b7a51de69db5b9c3c88c
Calvin <mrcalvinkc@gmail.com>
2017-12-06 18:41:34 -0800

    Added redux-devtools-chrome-extension

--- src/index.js
+++ src/index.js
@@ -8,8 +8,13 @@ import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 import reducers from './reducers';
 import 'dotenv/config';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(reducers, {}, applyMiddleware(promise, thunk));
+const store = createStore(
+  reducers,
+  {},
+  composeWithDevTools(applyMiddleware(promise, thunk))
+);
 ReactDOM.render(
   <Provider store={store}>
     <App />
