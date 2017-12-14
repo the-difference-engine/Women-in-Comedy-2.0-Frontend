3d44228587e69cda809c4df8d6e841b0d373e50e
Daniel Mai <danielmai1728@gmail.com>
2017-12-13 19:30:39 -0800

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
-// import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import promise from 'redux-promise';
 import thunk from 'redux-thunk';
 import registerServiceWorker from './registerServiceWorker';
+
 import App from './App';
 import reducers from './reducers';
-import 'dotenv/config';
-// import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(
-  reducers,
-  {},
-  // composeWithDevTools(applyMiddleware(promise, thunk))
-);
+const store = createStore(reducers, {}, applyMiddleware(promise, thunk));
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
