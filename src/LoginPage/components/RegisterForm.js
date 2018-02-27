b96cc449d028045b84ad363fcff6514da69096fd
Manita Herlitz-Ferguson <manita@Manitas-MacBook-Pro.local>
2018-02-26 18:24:54 -0600

    add photo upload and add material ui form fields

--- src/LoginPage/components/RegisterForm.js
+++ src/LoginPage/components/RegisterForm.js
@@ -1,157 +1,83 @@
-import React, {Component} from 'react';
-import {Field, reduxForm} from 'redux-form';
-import {connect} from 'react-redux';
-import axios from 'axios';
-
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import firebase from 'firebase'
+import axios from 'axios'
+// import {  createEvent, fetchUserInfo, eventInputChange } from '../../actions';
+import { TextField, RaisedButton, CircularProgress, SelectField, MenuItem, Label } from 'material-ui';
+import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
+import { TextValidator, ValidatorForm, SelectValidator } from 'react-material-ui-form-validator';
 import '../css/register.css'
+import createFragment from 'react-addons-create-fragment';
 
 class RegisterForm extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      userMade: false
+      user: {},
+      imgURL: null
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleDropdownChange = this.handleDropdownChange.bind(this);
+
+  }
+
+  handleChange(event) {
+    const { user } = this.state;
+    console.log(event.target.name);
+    user[event.target.name] = event.target.value;
+    this.state.user = user;
+  }
+
+  handleDropdownChange(event, index, value) {
+    console.log(event.target);
+    const { user } = this.state;
+    user.training = value;
+    this.setState({ user });
+    // console.log(this.state.user);
+  }
+
+  onClick() {
+    const input = document.getElementById('input');
+    input.click();
+  }
+
+  renderImg() {
+    if (this.state.imgURL) {
+      return <img id="img" src={this.state.imgURL} alt="" width="250" height="250" />
     }
   }
-  renderTitleField(field) {
-    const {
-      meta: {
-        touched,
-        error
-      }
-    } = field
-    const className = `form-group ${touched && error
-      ? 'has-danger'
-      : ''}`;
 
-    return (<div className={className} id="field-form">
-      <label>{field.label}</label>
-      <input className="form-control" type="text" {...field.input} placeholder={field.label}/>
-      <div className="text-help">
-        {
-          touched
-            ? error
-            : ''
-        }
-      </div>
-    </div>);
+  onUpload(event) {
+    const file = event.target.files;
+    const fileReader = new FileReader();
+
+    fileReader.readAsDataURL(file[0]);
+    fileReader.onload = () => {
+      this.setState({ imgURL: fileReader.result, img: file[0] });
+    };
   }
 
-  renderPasswordField(field) {
-    const {
-      meta: {
-        touched,
-        error
-      }
-    } = field
-    const className = `form-group ${touched && error
-      ? 'has-danger'
-      : ''}`;
-
-    return (<div className={className} id="field-form">
-      <label>{field.label}</label>
-      <input className="form-control" type="password" {...field.input} placeholder={field.label}/>
-      <div className="text-help">
-        {
-          touched
-            ? error
-            : ''
-        }
-      </div>
-    </div>);
-  }
-
-  renderGenderField(field) {
-    const {
-      meta: {
-        touched,
-        error
-      }
-    } = field
-    const className = `form-group ${touched && error
-      ? 'has-danger'
-      : ''}`;
-    return (<div className={className} id="field-form">
-      <label>Gender</label>
-      <div>
-        <label className="radio-inline">
-          <input type="radio" {...field.input} value="male"/>
-          Male
-        </label>
-        <label className="radio-inline">
-          <input type="radio" {...field.input} value="female"/>
-          Female
-        </label>
-      </div>
-      <div className="text-help">
-        {
-          touched
-            ? error
-            : ''
-        }
-      </div>
-    </div>);
-  }
-
-  renderMultipleChoice(field) {
-    const {
-      meta: {
-        touched,
-        error
-      }
-    } = field
-    const className = `form-group ${touched && error
-      ? 'has-danger'
-      : ''}`;
-    return (<div className={className}>
-      <label>{field.label}</label>
-      <div>
-        <div className="radio">
-          <label><input type="radio" {...field.input} value="less than 1 year"/>
-            less than 1 year</label>
-        </div>
-        <div className="radio">
-          <label><input type="radio" {...field.input} value="1-3 years"/>
-            1-3 years</label>
-        </div>
-        <div className="radio">
-          <label><input type="radio" {...field.input} value="4-7 years"/>
-            4-7 years</label>
-        </div>
-        <div className="radio">
-          <label><input type="radio" {...field.input} value="7-10 years"/>
-            7-10 years</label>
-        </div>
-        <div className="radio">
-          <label><input type="radio" {...field.input} value="11+ years"/>
-            11+ years
-          </label>
-        </div>
-      </div>
-      <div className="text-help">
-        {
-          touched
-            ? error
-            : ''
-        }
-      </div>
-    </div>);
-  }
-
-  renderCheckbox(field) {
-    return (<div>
-      <label className="checkbox-inline"><input type="checkbox" {...field.input}/>{field.label}</label>
-    </div>)
+  async storeProfilePicture() {
+    const ext = this.state.img.name.slice(this.state.img.name.lastIndexOf('.'));
+    const imageData = await firebase.storage().ref(`/users/${this.state.user.firstName + this.state.user.lastName}${ext}`).put(this.state.img);
+    this.state.user.photo = imageData.metadata.downloadURLs[0];
   }
 
   onSubmit(values) {
-    axios.post(process.env.REACT_APP_API_URL_DEV + 'users', values).then(payload => {
-      this.setState({userMade: true});
+    this.storeProfilePicture();
+    console.log("values: " + values);
+    axios.post("http://localhost:9000/api/v1/users", values).then(payload => {
+      this.setState({ userMade: true });
     }).catch(err => {
       alert(err)
     });
+    console.log(this.state.user);
+    this.setState({ userMade: true })
   }
   render() {
-    const {handleSubmit} = this.props;
+    const { handleSubmit } = this.props;
     if (this.state.userMade) {
       return (<div>
         Thank You For Signing Up for Women In Comedy! A confirmation email has been sent.
@@ -159,18 +85,189 @@ class RegisterForm extends Component {
     } else {
       return (<div id="register">
         <h1>Create a New Account</h1>
-        <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <div>
+          <div>
+
+            <ValidatorForm
+              // onSubmit={this.handleSubmit}
+              onSubmit={this.onSubmit}
+            >
+              <div>
+                <TextField
+                  floatingLabelText="First Name"
+                  onChange={this.handleChange}
+                  name="firstName"
+                  type="text"
+                  // value={this.state.user.firstName}
+                  // validators={['required']}
+                  // errorMessages={['this field is required']}
+                  underlineFocusStyle={{ display: 'none' }}
+                  floatingLabelFocusStyle={{ color: 'red' }}
+                />
+              </div>
+              <div>
+                <TextValidator
+                  floatingLabelText="Last Name"
+                  onChange={this.handleChange}
+                  name="lastName"
+                  type="text"
+                  // validators={['required']}
+                  // errorMessages={['this field is required']}
+                  underlineFocusStyle={{ display: 'none' }}
+                  floatingLabelFocusStyle={{ color: 'red' }}
+                />
+              </div>
+              <div>
+                <TextValidator
+                  floatingLabelText="Email Address"
+                  onChange={this.handleChange}
+                  name="email"
+                  type="text"
+                  // validators={['required', 'isEmail']}
+                  // errorMessages={['this field is required', 'email is not valid']}
+                  underlineFocusStyle={{ display: 'none' }}
+                  floatingLabelFocusStyle={{ color: 'red' }}
+                />
+              </div>
+              <div>
+                <TextValidator
+                  floatingLabelText="Password"
+                  onChange={this.handleChange}
+                  name="password"
+                  type="password"
+                  // validators={['required']}
+                  // errorMessages={['this field is required']}
+                  underlineFocusStyle={{ display: 'none' }}
+                  floatingLabelFocusStyle={{ color: 'red' }}
+                />
+              </div>
+              <div>
+                <TextValidator
+                  floatingLabelText="city"
+                  onChange={this.handleChange}
+                  name="city"
+                  type="text"
+                  // validators={['required']}
+                  // errorMessages={['this field is required']}
+                  underlineFocusStyle={{ display: 'none' }}
+                  floatingLabelFocusStyle={{ color: 'red' }}
+                />
+              </div>
+              <div>
+                <TextValidator
+                  floatingLabelText="Youtube Video"
+                  onChange={this.handleChange}
+                  name="video"
+                  type="text"
+                  // validators={['required']}
+                  // errorMessages={['this field is required']}
+                  underlineFocusStyle={{ display: 'none' }}
+                  floatingLabelFocusStyle={{ color: 'red' }}
+                />
+              </div>
+              <div>
+                <TextValidator
+                  floatingLabelText="Website"
+                  onChange={this.handleChange}
+                  name="website"
+                  type="text"
+                  // validators={['required']}
+                  // errorMessages={['this field is required']}
+                  underlineFocusStyle={{ display: 'none' }}
+                  floatingLabelFocusStyle={{ color: 'red' }}
+                />
+              </div>
+              <div>
+                <label>Gender</label>
+                <RadioButtonGroup 
+                  name="gender" 
+                  label="Gender"
+                  onChange={this.handleChange}>
+                  <RadioButton
+                    value="Female"
+                    label="Female"
+                  />
+                  <RadioButton
+                    value="Male"
+                    label="Male"
+                  />
+                </RadioButtonGroup>
+              </div>
+              <div>
+                <SelectField
+                  floatingLabelText="Years of Experience"
+                  onChange={(event, index, value) => {
+                    const { user } = this.state;
+                    user.experience = value;
+                    this.setState({ user });
+                  }}
+                  value={this.state.user.experience}
+                  name="training"
+                  underlineFocusStyle={{ display: 'none' }}
+                  floatingLabelFocusStyle={{ color: 'red' }}
+                >
+                  <MenuItem primaryText="Less than 1 year" value="Less than 1 year" />
+                  <MenuItem primaryText="1-3 years" value="1-3 years" />
+                  <MenuItem primaryText="4-7 years" value="4-7 years" />
+                  <MenuItem primaryText="7-10 years" value="7-10 years" />
+                  <MenuItem primaryText="11+ years" value="11+ years" />
+                </SelectField>
+              </div>
+              <div>
+                <SelectField
+                  floatingLabelText="Years of Comedy Training"
+                  onChange={(event, index, value) => {
+                    const { user } = this.state;
+                    user.training = value;
+                    this.setState({ user });
+                  }}
+                  value={this.state.user.training}
+                  name="training"
+                  underlineFocusStyle={{ display: 'none' }}
+                  floatingLabelFocusStyle={{ color: 'red' }}
+                >
+                  <MenuItem primaryText="Less than 1 year" value="Less than 1 year" />
+                  <MenuItem primaryText="1-3 years" value="1-3 years" />
+                  <MenuItem primaryText="4-7 years" value="4-7 years" />
+                  <MenuItem primaryText="7-10 years" value="7-10 years" />
+                  <MenuItem primaryText="11+ years" value="11+ years" />
+                </SelectField>
+              </div>
+              <RaisedButton
+                secondary
+                label="upload image"
+                onClick={this.onClick.bind(this)}
+              // disabled={loading}
+              />
+              <input type="file" id="input" style={{ display: 'none' }} onChange={this.onUpload.bind(this)} /><br />
+              {this.renderImg()}
+              <RaisedButton
+                secondary
+                label="register"
+                type="submit"
+                onClick={this.onSubmit.bind(this)}
+                style={{ marginTop: '15px' }}
+              // disabled={loading}
+              />
+            </ValidatorForm>
+          </div>
+
+
+
+
+          {/* <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
           <Field label="First name" name="firstName" component={this.renderTitleField}/>
           <Field label="Last name" name="lastName" component={this.renderTitleField}/>
           <Field label="Email" name="email" component={this.renderTitleField}/>
           <Field label="Password" name="password" component={this.renderPasswordField}/>
+          <Field label="Profile Picture" name="profilePhoto" component={this.renderImageUpload}/>
           <Field label="City" name="city" component={this.renderTitleField}/>
           <Field label="Video link to Youtube/Vimeo" name="video" component={this.renderTitleField}/>
           <Field label="Link to website" name="website" component={this.renderTitleField}/>
           <Field label="Gender" name="gender" value="male" component={this.renderGenderField}/>
+
           <Field label="Years of comedy training" name="training" component={this.renderMultipleChoice}/>
           <Field label="Years of professional comedy working in the industry" name="experience" component={this.renderMultipleChoice}/>
-
           <label>Avaliable to meet for</label>
           <Field label="Coffee" name="Coffee" component={this.renderCheckbox}/>
           <Field label="Feedback/Advice" name="Feedback/Advice" component={this.renderCheckbox}/>
@@ -180,8 +277,9 @@ class RegisterForm extends Component {
           <Field label="Seeking Mentors" name="Seeking Mentors" component={this.renderCheckbox}/>
 
           <button className="btn btn-success" type="submit">Submit</button>
-        </form>
+        </form> */}
 
+        </div>
       </div>);
     }
   }
@@ -223,4 +321,4 @@ function validate(values) {
   return errors;
 }
 
-export default reduxForm({validate, form: 'newAccountForm'})(connect(null)(RegisterForm));
+export default reduxForm({ validate, form: 'newAccountForm' })(connect(null)(RegisterForm));
