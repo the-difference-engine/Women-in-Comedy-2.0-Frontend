e32f07abede76c2984418fc196112c58ab26bdbc
Caroline <carolinegferris@gmail.com>
2018-04-19 18:19:27 -0500

    fix styling of the alert button

--- src/containers/notification_button.js
+++ src/containers/notification_button.js
@@ -1,53 +1,5 @@
 import React from 'react';
 
-// class NotificationButton extends Component {
-
-// constructor(props) {
-//   super(props);
-// }
-//
-// componentDidMount() {
-//   const userId = sessionStorage.getItem('userId');
-//   const {fetchNotifications} = this.props;
-//   fetchNotifications(userId);
-//
-// }
-
-
-// render() {
-//
-//   return (
-//
-//     <a href='#'className="icon"><i className="fa fa-bell-o"><span className="badge">{this.props.notifications.length}</span><p>ALERTS </p></i></a>
-//     );
-// }
-
-
-// function mapDispatchToProps(dispatch) {
-//   return bindActionCreators({ fetchNotifications }, dispatch);
-// }
-
-// const mapStateToProps = state => {
-//     const {notifications} = state;
-//     return {notifications};
-//
-// };
-
-// export default connect(null, mapDispatchToProps)(NotificationButton);
-// export default connect(mapStateToProps, {fetchNotifications})(NotificationButton);
-
-
-// };
-
-// export default (props) => {
-//
-//     return (
-//         debugger;
-//       <a href='#' className="icon"><i className="fa fa-bell-o"><span className="badge">{this.props.notifications.length}</span><p>ALERTS </p></i></a>
-//       );
-// };
-// export default connect(null, {fetchNotifications})(NotificationButton);
-
 export default (props) => {
     if (props.notifications === null) {
         return <div>
@@ -57,41 +9,22 @@ export default (props) => {
 
     function findConnectionActions(props) {
         if (props.notifications !== null) {
-            var connectionAccepted = props.notifications.notifications.filter(not => not.action === "connection_accepted");
-            return connectionAccepted.length;
+            const connectionAccepted = props.notifications.notifications.filter(notification => notification.action === "connection_accepted")
+                .filter(notification => notification.seen === null);
+
+            if (connectionAccepted.length === 0) {
+                return null;
+            } else {
+                return connectionAccepted.length;
+            }
         }
     }
 
     return (
         <div>
-            <a href={'/notifications'} className="icon"><i className="fa fa-bell-o"><span
+            <a href={'/notifications'} className="icon"><i className="fa fa-bell-o" id="alert-button"><span
                 className="badge">{findConnectionActions(props)}</span><p>ALERTS </p></i></a>
         </div>
     );
 };
-
-
-// const NotificationButton = (props) => {
-//     const { notifications} = props;
-//
-//     return (
-//         <div>
-//             {notificationPopUp(notifications)}
-//                 </div>
-//     );
-// };
-{/*//*/
-}
-{/*// const notificationPopUp = ({notifications}) => {*/
-}
-//     if (!notifications) {
-//         return <a href='#' className="icon"><i className="fa fa-bell-o"><p>ALERTS </p></i></a>
-//     }
-//     return (
-//         <a href='#' className="icon"><i className="fa fa-bell-o"><span className="badge">123</span>
-//             <p>ALERTS </p></i></a>
-//     );
-// };
-//
-// export default NotificationButton;
 
