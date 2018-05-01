fd0f79616c63dcd7c4b7c36e557f461a53b07fc2
Caroline <carolinegferris@gmail.com>
2018-04-30 19:34:18 -0500

    resolve conflict pt 2

--- src/EventsFeedPage/EventsFeed.js
+++ src/EventsFeedPage/EventsFeed.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {
-  fetchEventInfo,
+    fetchEventInfo,
     fetchHostPhoto,
-  attendEvent,
-  fetchUserInfo,
-  unattendEvent,
-  eventWallInputChange,
-  createPostOnEventWall
+    attendEvent,
+    fetchNotifications,
+    fetchUserInfo,
+    unattendEvent,
+    eventWallInputChange,
+    createPostOnEventWall
 } from '../actions';
 import Guests from './components/Guests';
 import NewFeeds from './components/NewFeeds';
@@ -18,90 +19,98 @@ import UpdateEvent from './components/UpdateEvent';
 import {LeftGraySideBar, PageContent, RightGraySideBar} from '../common';
 
 class EventsFeed extends Component {
-  constructor(props) {
-    super(props);
-  }
-  async componentWillMount() {
-    const currentUserId = await sessionStorage.getItem('userId');
-    this.props.fetchUserInfo(currentUserId);
-    const eventId = this.props.match.params.id;
-    await this.props.fetchEventInfo(eventId);
-    const hostId = this.props.selectedEvent.info.user_id;
-    await this.props.fetchHostPhoto(hostId);
-  }
-  onCreatePost() {
-    const body = this.props.eventWallPost;
-    const eventId = this.props.match.params.id;
-    const authorId = sessionStorage.getItem('userId');
-    this.props.createPostOnEventWall({ body, eventId, authorId}, this.props.fetchEventInfo);
-  }
-  render() {
-    return (
-      <div id="events-feed-container">
-        <div>
-          <Navbar history={this.props.history} />
-        </div>
-        <div id="pic-wrap">
-          <EventImg event={this.props.selectedEvent} />
-        </div>
+    constructor(props) {
+        super(props);
+    }
 
-          <LeftGraySideBar className="event-info-bar">
-            <EventInfo
-              event={this.props.selectedEvent}
-              attendEvent={this.props.attendEvent}
-              userInfo={this.props.userInfo}
-              eventId={this.props.match.params.id}
-              fetchEventInfo={this.props.fetchEventInfo}
-              unattendEvent={this.props.unattendEvent}>
-            </EventInfo>
-          <UpdateEvent id="edit_btn" history={this.props.history} eventId={this.props.match.params.id}/>
+    async componentWillMount() {
+        const currentUserId = await sessionStorage.getItem('userId');
+        this.props.fetchUserInfo(currentUserId);
+        const eventId = this.props.match.params.id;
+        await this.props.fetchEventInfo(eventId);
+        const hostId = this.props.selectedEvent.info.user_id;
+        await this.props.fetchHostPhoto(hostId);
+    }
 
-          </LeftGraySideBar>
+    onCreatePost() {
+        const body = this.props.eventWallPost;
+        const eventId = this.props.match.params.id;
+        const authorId = sessionStorage.getItem('userId');
+        this.props.createPostOnEventWall({body, eventId, authorId}, this.props.fetchEventInfo);
+    }
 
-          <PageContent pageTitle={"Event Feed"} className="event-feed"><h4>Event Feed</h4>
-            <div className="feed-post-bar">
-              <div className="wrap">
-                <div className="search">
-                  <input
-                    type="text"
-                    className="searchTerm"
-                    placeholder="What's New?"
-                    onChange={event => this.props.eventWallInputChange(event.target.value)}
-                    value={this.props.eventWallPost}
-                  />
-                  <div className="post-button"><button className="btn btn-default" onClick={this.onCreatePost.bind(this)}>POST</button></div>
+    render() {
+        return (
+            <div id="events-feed-container">
+                <div>
+                    <Navbar history={this.props.history}/>
                 </div>
-              </div>
+                <div id="pic-wrap">
+                    <EventImg event={this.props.selectedEvent}/>
+                </div>
+
+                <LeftGraySideBar className="event-info-bar">
+                    <EventInfo
+                        event={this.props.selectedEvent}
+                        attendEvent={this.props.attendEvent}
+                        userInfo={this.props.userInfo}
+                        eventId={this.props.match.params.id}
+                        fetchEventInfo={this.props.fetchEventInfo}
+                        unattendEvent={this.props.unattendEvent}>
+                    </EventInfo>
+                    <UpdateEvent id="edit_btn" history={this.props.history} eventId={this.props.match.params.id}/>
+
+                </LeftGraySideBar>
+
+                <PageContent pageTitle={"Event Feed"} className="event-feed"><h4>Event Feed</h4>
+                    <div className="feed-post-bar">
+                        <div className="wrap">
+                            <div className="search">
+                                <input
+                                    type="text"
+                                    className="searchTerm"
+                                    placeholder="What's New?"
+                                    onChange={event => this.props.eventWallInputChange(event.target.value)}
+                                    value={this.props.eventWallPost}
+                                />
+                                <div className="post-button">
+                                    <button className="btn btn-default" onClick={this.onCreatePost.bind(this)}>POST
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <NewFeeds event={this.props.selectedEvent} className="event-comment"/>
+
+                </PageContent>
+
+
+                <div id="container">
+                    <RightGraySideBar>
+                        <Guests
+                            event={this.props.selectedEvent}
+                        />
+                    </RightGraySideBar>
+                </div>
             </div>
-
-            <NewFeeds event={this.props.selectedEvent} className="event-comment"/>
-
-          </PageContent>
-
-
-          <div id="container">
-            <RightGraySideBar>
-              <Guests
-                event={this.props.selectedEvent}
-              />
-            </RightGraySideBar>
-          </div>
-      </div>
-    );
-  }
+        );
+    }
 }
-function mapStateToProps({ selectedEvent, userInfo, eventWallPost }) {
-  return { selectedEvent, userInfo, eventWallPost };
+
+function mapStateToProps({selectedEvent, userInfo, eventWallPost}) {
+    return {selectedEvent, userInfo, eventWallPost};
 }
+
 export default connect(mapStateToProps,
-  {
-    fetchEventInfo,
-    attendEvent,
-    fetchUserInfo,
-      fetchNotifications,
-      fetchHostPhoto,
-    unattendEvent,
-    eventWallInputChange,
-    createPostOnEventWall
-  }
+    {
+        fetchEventInfo,
+        attendEvent,
+        fetchUserInfo,
+        fetchNotifications,
+        fetchHostPhoto,
+        unattendEvent,
+        eventWallInputChange,
+        createPostOnEventWall
+    }
 )(EventsFeed);
