906c1312d429bf17e90558f4ae617c6c9743123d
Manita Herlitz-Ferguson <manita.herlitzferguson@gmail.com>
2018-05-09 19:28:33 -0500

    fix user block actions urls

--- src/actions/user_block_actions.js
+++ src/actions/user_block_actions.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
-import { FETCH_BLOCKED_USERS, FETCH_BLOCKED_BY} from './types';
+import { FETCH_BLOCKED_USERS, FETCH_BLOCKED_BY } from './types';
 
 export const fetchBlockedUsers = (userId) => {
  const request = axios({
    method: 'get',
-   url: process.env.REACT_APP_API_URL_DEV + '/users/blocked',
+   url: process.env.REACT_APP_API_URL_DEV + 'users/blocked',
    headers: {"id": userId},
  });
  return (dispatch) => {
@@ -17,7 +17,7 @@ export const fetchBlockedUsers = (userId) => {
 export const fetchBlockedBy = (userId) => {
     const request = axios({
       method: 'get',
-      url: process.env.REACT_APP_API_URL_DEV + '/users/blockedby',
+      url: process.env.REACT_APP_API_URL_DEV + 'users/blockedby',
       headers: {"id": userId},
     });
     return (dispatch) => {
