397f691a98693422f4bf9136e0831f98387a1857
digitalh2o <digitalh2o@gmail.com>
2018-06-06 19:10:54 -0500

    Can now load the admin page once adding new conditional

--- src/containers/notification_button.js
+++ src/containers/notification_button.js
@@ -1,30 +1,45 @@
-import React from 'react';
+import React from "react";
 
-export default (props) => {
-    if (props.notifications === null) {
-        return <div>
-            <a href={'/notifications'} className="icon"><i className="fa fa-bell-o"><p>ALERTS </p></i></a>
-        </div>
-    }
-
-    function findConnectionActions(props) {
-        if (props.notifications !== null) {
-            const connectionAccepted = props.notifications.notifications.filter(notification => notification.action === "connection_accepted"
-                || notification.action === "connection_request").filter(notification => notification.seen === null);
-
-            if (connectionAccepted.length === 0) {
-                return null;
-            } else {
-                return connectionAccepted.length;
-            }
-        }
-    }
-
+export default props => {
+  if (props.notifications === null || props.notifications === undefined) {
     return (
-        <div>
-            <a href={'/notifications'} className="icon"><i className="fa fa-bell-o" id="alert-button"><span
-                className="badge">{findConnectionActions(props)}</span><p>ALERTS </p></i></a>
-        </div>
+      <div>
+        <a href={"/notifications"} className="icon">
+          <i className="fa fa-bell-o">
+            <p>ALERTS </p>
+          </i>
+        </a>
+      </div>
     );
-};
+  }
 
+  function findConnectionActions(props) {
+    if (props.notifications !== null) {
+      console.log("what im working with", props);
+      const connectionAccepted = props.notifications.notifications
+        .filter(
+          notification =>
+            notification.action === "connection_accepted" ||
+            notification.action === "connection_request"
+        )
+        .filter(notification => notification.seen === null);
+
+      if (connectionAccepted.length === 0) {
+        return null;
+      } else {
+        return connectionAccepted.length;
+      }
+    }
+  }
+
+  return (
+    <div>
+      <a href={"/notifications"} className="icon">
+        <i className="fa fa-bell-o" id="alert-button">
+          <span className="badge">{findConnectionActions(props)}</span>
+          <p>ALERTS </p>
+        </i>
+      </a>
+    </div>
+  );
+};
