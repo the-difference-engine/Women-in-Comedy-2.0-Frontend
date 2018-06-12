beda2add413c25d25bdbfd1a260f3274f3cac142
digitalh2o <digitalh2o@gmail.com>
2018-06-11 19:53:24 -0500

    Working on the admin form when user is an admin or not.

--- src/containers/notification_button.js
+++ src/containers/notification_button.js
@@ -1,49 +1,5 @@
 import React from "react";
 
-<<<<<<< HEAD
-export default props => {
-  if (props.notifications === null || props.notifications === undefined) {
-    return (
-      <div>
-        <a href={"/notifications"} className="icon">
-          <i className="fa fa-bell-o">
-            <p>ALERTS </p>
-          </i>
-        </a>
-      </div>
-    );
-  }
-
-  function findConnectionActions(props) {
-    if (props.notifications !== null) {
-      console.log("what im working with", props);
-      const connectionAccepted = props.notifications.notifications
-        .filter(
-          notification =>
-            notification.action === "connection_accepted" ||
-            notification.action === "connection_request"
-        )
-        .filter(notification => notification.seen === null);
-
-      if (connectionAccepted.length === 0) {
-        return null;
-      } else {
-        return connectionAccepted.length;
-      }
-    }
-  }
-
-  return (
-    <div>
-      <a href={"/notifications"} className="icon">
-        <i className="fa fa-bell-o" id="alert-button">
-          <span className="badge">{findConnectionActions(props)}</span>
-          <p>ALERTS </p>
-        </i>
-      </a>
-    </div>
-  );
-=======
 export default (props) => {
   const findConnectionActions = (props) => {
     const connectionAccepted = props.notifications.notifications.filter(notification => notification.action === "connection_accepted"
@@ -73,5 +29,4 @@ export default (props) => {
       </div>
     );
   }
->>>>>>> qa
 };
