5a6d19ce4d63230ad0540911ca79fc44759e1ab1
digitalh2o <digitalh2o@gmail.com>
2018-06-20 20:27:56 -0500

    fixed conflicts

--- src/AdminPage/components/EmailModal.js
+++ src/AdminPage/components/EmailModal.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
 import { Dialog, TextField } from "material-ui";
 import { connect } from "react-redux";
@@ -46,47 +45,3 @@ export default connect(
   null,
   { mailUsers }
 )(EmailModal);
-=======
-import React, {Component} from "react";
-import {Dialog, TextField} from "material-ui";
-
-class EmailModal extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            open: false
-        };
-        this.handleOpen = this.handleOpen.bind(this);
-        this.handleClose = this.handleClose.bind(this);
-    };
-
-    handleOpen() {
-        this.setState({open: true});
-    };
-
-    handleClose() {
-        this.setState({open: false});
-    };
-
-    render() {
-        return(
-            <div>
-                <br/>
-                <button onClick={this.handleOpen}>Email Users</button>
-                <Dialog
-                    autoScrollBodyContent={true}
-                    modal={false}
-                    open={this.state.open}
-                    onRequestClose={this.handleClose}
-                >
-                <textarea></textarea>
-                <br/>
-                <button>Submit</button>
-                </Dialog>
-            </div>
-        );
-    }
-}
-
-export default EmailModal;
->>>>>>> 2a4044dde8b420cdf1df264bf55cd42008b77a9d
