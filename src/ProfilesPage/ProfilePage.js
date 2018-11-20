39654fa32d3f2549bc4ff6d9357c1eb72a33c782
DeAndre Boston <dboston3@yahoo.com>
2018-11-19 20:43:32 -0600

    onDelete function fix and component didMount

--- src/ProfilesPage/ProfilePage.js
+++ src/ProfilesPage/ProfilePage.js
@@ -33,14 +33,12 @@ const admin = sessionStorage.getItem("isAdmin");
 
 class ProfilePage extends Component {
 
-  componentWillMount() {
-    constructor(props) {
-      super(props);
+  constructor(props) {
+    super(props);
 
-      this.state = {
-        deleteModalVisible: false
-      };
-    }
+    this.state = {
+      deleteModalVisible: false
+    };
   }
 
   componentDidMount() {
@@ -154,7 +152,7 @@ class ProfilePage extends Component {
   }
 
   // @TODO What are we deleting? Please be more specific naming functions
-  onDelete() => {
+  onDelete() {
     const id = this.props.match.params.id || sessionStorage.getItem("userId");
     this.props.deleteUser(id);
     this.props.history.push('/message');
