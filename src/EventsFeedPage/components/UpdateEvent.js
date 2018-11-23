d65351971f59497b4fe42d83ed7456b26ee702e8
Meg Glavey <glaveym@gmail.com>
2018-11-23 13:45:25 -0600

    reviewing front end code for exisiting update event is current user is admin logic

--- src/EventsFeedPage/components/UpdateEvent.js
+++ src/EventsFeedPage/components/UpdateEvent.js
@@ -8,6 +8,9 @@ class UpdateEvent extends Component {
   render() {
     return(
         // TODO: if current user = event user id
+        // if current user = admin, then render edit event button on all events
+        // if current user /= admin, then render edit event button only on that user's event
+          
           <div className="edit_btn"><a className="btn btn-default" href={`/updateevent/${this.props.eventId}`}>EDIT THIS EVENT</a>
           </div>
     ); 
