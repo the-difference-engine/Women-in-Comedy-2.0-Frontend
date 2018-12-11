e7b70d3ac1080f87abd6822a2bea412192a488eb
Vanessa <vesterbrook@gmail.com>
2018-12-10 19:19:56 -0600

    deleted the code to view posts

--- src/FeedPage/FeedPage.js
+++ src/FeedPage/FeedPage.js
@@ -59,17 +59,6 @@ class Feed extends Component {
           <UserInfo userInfo={userInfo} userConnections={userConnections} />
         </LeftGraySideBar>
         <PageContent>
-          {/* <div className="feed-post-bar">
-            <div className="wrap">
-              <div className="search">
-                <input type="text" className="searchTerm" placeholder="What's New?"
-                  onChange={(event) => this.props.userWallInputChange(event.target.value)}
-                  value={this.props.userWallPost}/>
-                <div className="post-button"><button className="btn btn-default" onClick={this.onPost.bind(this)}>POST</button></div>
-              </div>
-            </div>
-          </div>
-          <NewFeeds userFeeds={userFeeds} /> */}
         </PageContent>
       </div>
     );
