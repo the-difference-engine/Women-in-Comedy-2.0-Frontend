d6b2920c6566f6ee7f00a0fa0c89aef3e39bb85f
Michael Kirlin <mkirlin@gmail.com>
2018-12-17 20:11:11 -0600

    Update LandingPage.js

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -50,7 +50,7 @@ class LandingPage extends Component {
         return(
                 
             <div className = 'container' id='background'>
-                <div id='landingPageRow'className='row'>
+                <div id='landingPageRow' className='row'>
                     <div className='col-lg-12 colBackground'>
 
                         <h1 id='landingPageTitle'>Women in <span style={{ color: 'rgba(254, 8, 101, 1)' }}>Com</span><span style={{ color: 'rgba(209, 13, 13, 1)' }}>edy</span></h1>
